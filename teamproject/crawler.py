d5f17088379627812d604e01a2240538df0aae64
Sebastian Kralik <sebastian.kralik@student.uni-tuebingen.de>
2021-11-07 13:48:26 +0100

    updated crawler, added example: matches.json

--- teamproject/crawler.py
+++ teamproject/crawler.py
@@ -7,6 +7,7 @@ import json, requests, typing
 """
 fetch_data() fetches match data in a given interval
 For example call fetch_data(2020,1,2020,8)
+For example call fetch_data(2020,0,0,0)
 The result is a list of matches. A match is a dictionary structured as follows:
 {
     "date": "YYYY-MM-DDTHH:MM:SSZ",
@@ -29,61 +30,59 @@ def fetch_data(startYear:int, startDay:int, endYear:int, endDay:int) -> None:
     # check validity of time span, return empty list if invalid time span
     if (startYear > endYear) or (startYear == endYear and startDay > endDay):
         print("Invalid time span. No data fetched.")
-        return matchesOutput
+        return None
 
     # specify parameters for api fetches:
-    # apiScheme defines the fundamental query scheme
+    # apiScheme defines the fundamental query mode
     # leaguesOfInterest specifys a list of league short names
     # see: https://www.openligadb.de/Datenhaushalt/
     # yearsOfInterest is a list of all selected years
     apiScheme = "https://api.openligadb.de/getmatchdata/"
-    leaguesOfInterest = ["bl"+str(n+1) for n in range(3)]
     yearsOfInterest = range(startYear, endYear+1)
-    day = startDay
+    leaguesOfInterest = ["bl"+str(n+1) for n in range(3)]
 
-    # loop over leagues, years, days and fetch data records
-    for league in leaguesOfInterest:
-        for year in yearsOfInterest:
-            # loop over days until empty fetch or specified endDay reached
-            while not(year == endYear and day > endDay):
-                # fetch JSON data from openligadb.de
-                url = apiScheme + league + "/" + str(year) + "/" + str(day)
-                response = requests.get(url,headers={"Accept":"application/json"})
-                matchesFetched = json.loads(response.text)
-                print("Fetching " + url)
+    # loop over years, leagues and fetch matches
+    for year in yearsOfInterest:
+        for league in leaguesOfInterest:
+            # fetch JSON data from openligadb.de
+            url = apiScheme + league + "/" + str(year)
+            response = requests.get(url, headers={"Accept": "application/json"})
+            matchesFetched = json.loads(response.text)
+            print("Fetching " + url)
 
-                # break loop if there is no data for a day
-                # (continue with next year)
-                if len(matchesFetched) == 0:
-                    break
-                # day increment after every api fetch
-                day += 1
+            # check if season is finished
+            matchesUnfinished = [m for m in matchesFetched if not m["matchIsFinished"]]
+            if (len(matchesUnfinished) > 0):
+                pass # skip season? TO-DO
 
-                # convert input JSON from openligadb to internal format
-                for match in matchesFetched:
-                    # get final score, which is the last element of 'goals' list
-                    goalsFinalScore = match["goals"][-1:]
-                    # if 'goals' list is empty, set final score 0:0
-                    if not(goalsFinalScore):
-                        goalsHomeClub = 0
-                        goalsVisitingClub = 0
-                    else:
-                        goalsHomeClub = goalsFinalScore[0]["scoreTeam1"]
-                        goalsVisitingClub = goalsFinalScore[0]["scoreTeam2"]
-                    # create a new JSON-object which represents a match
-                    matchesOutput.append({
-                        "date": match["matchDateTimeUTC"],
-                        "homeClub": match["team1"]["shortName"],
-                        "visitingClub": match["team2"]["shortName"],
-                        "goalsHomeClub": goalsHomeClub,
-                        "goalsVisitingClub": goalsVisitingClub
-                    })
+            # only consider finished matches within specified interval
+            matchesOfInterest = [m for m in matchesFetched if m["matchIsFinished"] and not(year == endYear and m["group"]["groupOrderID"] > endDay)]
 
-            # having fetched a year, reset day counter to the first day
-            day = 1
+            # convert input JSON from openligadb to internal format
+            # only loop over finished matches
+            for match in matchesOfInterest:
+                # get final score, which is the last element of 'goals' list
+                goalsFinalScore = match["goals"][-1:]
+                # if 'goals' list is empty, set final score to 0:0
+                if not(goalsFinalScore):
+                    goalsHomeClub = 0
+                    goalsVisitingClub = 0
+                else:
+                    goalsHomeClub = goalsFinalScore[0]["scoreTeam1"]
+                    goalsVisitingClub = goalsFinalScore[0]["scoreTeam2"]
+                # append a new JSON-object which represents a match
+                matchesOutput.append({
+                    "date": match["matchDateTimeUTC"],
+                    "homeClub": match["team1"]["shortName"],
+                    "visitingClub": match["team2"]["shortName"],
+                    "goalsHomeClub": goalsHomeClub,
+                    "goalsVisitingClub": goalsVisitingClub
+                })
 
     # write result to file
-    with open("data-placeholder.json", "w") as outfile:
+    with open("matches.json", "w") as outfile:
         json.dump(matchesOutput, outfile)
 
     return None
+
+fetch_data(2020,1,2022,1)
