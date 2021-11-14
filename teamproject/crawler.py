73d78ff178d2912442009dd95e61a55b715554fc
Sebastian Kralik <sebastian.kralik@student.uni-tuebingen.de>
2021-11-14 20:35:26 +0100

    copy files

--- teamproject/crawler.py
+++ teamproject/crawler.py
@@ -2,107 +2,132 @@
 This module contains code to fetch required data from the internet and convert
 it to our internal format.
 """
-import json, re, requests, typing
+import json, os, requests, typing
+import pandas as pd
 
-"""
-fetch_data() fetches match data within a given interval. The resulting is a list
-matches. A match is a dictionary of the following structure:
-{
-    "date": "YYYY-MM-DDTHH:MM:SSZ",
-    "homeClub": str,
-    "guestClub": str,
-    "homeScore": int,
-    "guestScore": int
-}
-where "date" specifies the date and time a match took place in UTC as indicated
-by letter "Z" (zero meridian) at the end of the string,
-"homeClub" and "guestClub" represent the names of the home and visiting club,
-"homeScore" and "guestScore" represent the final goals score of each club.
-For example call:
-data = fetch_data(2020,1,2020,8)
-with open("matches.json", "w") as file:
-    json.dump(data, file)
-
-ggf. weitere wichtige Daten:
-• Liga
-• Ort
-• Tore: Spielminute, Torschütze, Eigentor?, Strafe?, Verlängerung?
-• Wetter (von woanders)
-"""
-def fetch_data(startYear:int, startDay:int, endYear:int, endDay:int) -> list:
-    # preconditions: check validity of time span
-    assert startYear <= endYear, "Invalid year interval"
-    assert (startYear < endYear or startDay < endDay), "Invalid day interval"
-
-    # matchesOutput is the resulting list of matches
-    matchesOutput = []
-
-    # specify parameters for api fetches:
-    # apiScheme determines the fundamental query mode
-    # leaguesOfInterest is a list of str representing league identifiers
-    # (see: https://www.openligadb.de/Datenhaushalt)
-    # yearsOfInterest is a list of int representing years
-    apiScheme = "https://api.openligadb.de/getmatchdata/"
-    yearsOfInterest = range(startYear, endYear+1)
-    leaguesOfInterest = ["bl"+str(n+1) for n in range(3)]
-
-    for year in yearsOfInterest:
-        for league in leaguesOfInterest:
-            # fetch json data from openligadb.de
-            url = apiScheme + league + "/" + str(year)
-            response = requests.get(url, headers={"Accept": "application/json"})
-            matchesFetched = json.loads(response.text)
-            print("Fetching " + url)
-
-            # check if season contains unfinished matches
-            if len([m for m in matchesFetched if not m["matchIsFinished"]]) > 0:
-                pass # TO-DO: skip unfinished season?
-
-            # only consider finished matches within specified interval
-            matchesOfInterest = [m for m in matchesFetched
-                if m["matchIsFinished"]
-                and not(year==endYear and m["group"]["groupOrderID"] > endDay)]
-
-            # convert openligadb json format to internal format
-            for match in matchesOfInterest:
-                # get final score, which is the last element of the list 'goals'
-                # set final score to 0:0 if 'goals' is an empty list
-                finalScore = match["goals"][-1:]
-                if finalScore:
-                    homeScore = finalScore[0]["scoreTeam1"]
-                    guestScore = finalScore[0]["scoreTeam2"]
-                else:
-                    scoreHome = 0
-                    guestScore = 0
-                # add current match to the resulting list of matches
-                matchesOutput.append({
-                    "date": match["matchDateTimeUTC"],
-                    "homeClub": match["team1"]["teamName"],
-                    "guestClub": match["team2"]["teamName"],
-                    "homeScore": homeScore,
-                    "guestScore": guestScore
-                })
-
-    # postconiditions: ensure valid and consistent data format
-    #assert all(isinstance(m["date"],str) for m in matchesOutput)
-    for m in matchesOutput:
-        assert isinstance(m["date"], str)
-        assert re.search("\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z", m["date"]), (
-            "Invalid date format")
-        assert isinstance(m["homeClub"], str)
-        assert isinstance(m["guestClub"], str)
-        assert len(m["homeClub"]) > 0 and len(m["guestClub"]) > 0, (
-            "Invalid team name")
-        assert isinstance(m["homeScore"], int)
-        assert isinstance(m["guestScore"], int)
-        assert m["homeScore"] >= 0 and m["guestScore"] >= 0, (
-            "Invalid number of goals")
-
-    # return resulting list of matches
-    return matchesOutput
+def api_query(param:str):
+    """
+    Sends a request to openligadb.de and parses json response.
+        Parameters:
+            param (str): Parameters in the request url [see api.openligadb.de]
+        Returns:
+            data (list, dict): JSON data parsed to python list (or dict)
+        Examples:
+            api_query("getavailableleagues")
+            api_query("getavailablegroups/bl1/2020")
+            api_query("getmatchdata/bl1/2020")
+            api_query("getmatchdata/bl1/2020/8")
+            More see
+    """
+    assert len(param) > 0, "Specified parameter too short."
+    url = f"https://api.openligadb.de/{param}"
+    response = requests.get(url, headers={"Accept": "application/json"})
+    data = json.loads(response.text)
+    return data
 
 
+def avail_data(details:str = "all") -> list[dict]:
+    """
+    Returns all bundesliga data available on openligadb.de.
+        Parameters:
+            details (str): Specify "league-only" to skip fetching matchdays
+        Returns:
+            data (list[dict]): A sorted list of leagues (dicts)
+    """
+    # get all available leagues of bundesliga
+    fetchedLeagues = api_query("getavailableleagues")
+    availLeagues = [{"ID": l["leagueID"], "season": int(l["leagueSeason"]),
+        "division": l["leagueShortcut"]} for league in fetchedLeagues
+        if league["leagueShortcut"] in ["bl1", "bl2", "bl3"]]
+    # add available match days to leagues if desired
+    if details == "all":
+        for league in availLeagues:
+            fetchedGroups = api_query("getavailablegroups/" +
+                f"{league['division']}/{league['season']}")
+            league["matchdays"] = [g["groupOrderID"] for g in fetchedGroups]
+    return sorted(availableLeagues, key=lambda l: l["season"])
 
-data = fetch_data(2020,1,2020,8)
-with open("matches.json", "w") as file:
-    json.dump(data, file)
+
+def parse_match(match:dict) -> dict:
+    """
+    Converts a match from openligadb.de format into internal format.
+        Parameters:
+            match (dict): Detailed match data from openligadb
+        Returns:
+            match (dict): Reduced match data
+    """
+    return {
+        "date": match["matchDateTime"],
+        "homeClubId": match["team1"]["teamId"],
+        "homeClub": match["team1"]["teamName"],
+        "guestClubId": match["team2"]["teamId"],
+        "guestClub": match["team2"]["teamName"],
+        "homeScore": match["matchResults"][0]["pointsTeam1"],
+        "guestScore": match["matchResults"][0]["pointsTeam2"]
+    }
+
+
+def fetch_data(startYear:int, startDay:int, endYear:int, endDay:int) -> str:
+    """
+    Fetches all bundesliga matches within a given interval.
+        Parameters:
+            startYear (int): Year of lower interval limit
+            startDay (int): Day of lower interval limit
+            endYear (int): Year of upper interval limit
+            endDay (int): Day of upper interval limit
+        Returns:
+            filePath (str): Path to json file containing all fetched data
+        Example:
+            filePath = fetch_data(2020,1, 2020,38)
+            with open(filePath) as file: matches = json.loads(file.read())
+            matchesDF = pd.DataFrame.read_json(filePath)
+    """
+    # preconditions: check validity of given time interval
+    assert startYear > 0 and endYear > 0, "Years must be greater than 0."
+    assert startYear <= endYear, "Invalid year interval."
+    assert startDay >= 0 and endDay >= 0, "Days must be positive integers."
+    assert (startYear < endYear or startDay < endDay), "Invalid day interval."
+    # matches is the resulting list of matches
+    matches = []
+    filePath = f"{os.path.dirname(__file__)}/crawled_data/" + (
+        f"matches-{startYear}-{startDay}-{endYear}-{endDay}.json")
+    # check if data has already been crawled before
+    try:
+        f = open(filePath)
+        f.close()
+        return filePath
+    except IOError:
+        pass
+
+    for year in range(startYear, endYear+1):
+        for division in ["bl1","bl2","bl3"]:
+            # parse all finished matches within specified interval
+            matchesFetched = api_query(f"getmatchdata/{division}/{year}")
+            matchesOfInterest = [parse_match(m) for m in matchesFetched
+                if (year < endYear or m["group"]["groupOrderID"] <= endDay)
+                and m["matchIsFinished"]]
+            matches += matchesOfInterest
+    # save resulting list of matches as json file
+    with open(filePath, "w") as f:
+        json.dump(matches, f)
+    return filePath
+
+
+def fetch_seasons(numSeasons:int) -> str:
+    """
+    Fetches all bundesliga matches of the last finished seasons.
+        Parameters:
+            numSeasons (int): Number of seasons that should be fetched
+        Returns:
+            filePath (str): Path to json file containing all fetched data
+    """
+    latestLeague = avail_data("leagues-only")[-1:]
+    assert latestLeague, "No available leagues."
+    return fetch_data(latestLeague-numSeasons-1, 1, latestLeague, 0)
+
+
+# ggf. weitere wichtige Daten: Liga; Ort; Tore (Spielminute, Torschütze,
+# Eigentor?, Strafe?, Verlängerung?); Wetter (von woanders)
+
+#print(fetch_data(2020,1, 2020,38))
+#print(avail_data())
