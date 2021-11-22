1080187792555000252f349ae45f954ce611a5ab
Philipp Wagner <ph.wagner@student.uni-tuebingen.de>
2021-11-22 21:39:24 +0100

    modifiied data_analytics.py with more statistic examples

--- teamproject/data_analytics.py
+++ teamproject/data_analytics.py
@@ -31,9 +31,22 @@ Example:
 for match in fetchedMatches:
     print(match["homeScore"])
 """
-#list of teams in Bundesliga
+#testing list of teams in Bundesliga
 teams = ["VfL Wolfsburg", "Borussia Dortmund", "1. FC Nürnberg", "Werder Bremen", "Hertha BSC", "1. FSV Mainz 05", "TSG 1899 Hoffenheim", "VfL Bochum", "SC Freiburg", "FC Bayern München", "VfB Stuttgart", "Hamburger SV",
          "Bayer Leverkusen", "Hannover 96", "Eintracht Frankfurt", "1. FC Köln", "Borussia Mönchengladbach", "FC Schalke 04" ]
+allTeams = []
+dublicate = False
+for match in fetchedMatches:
+    for team in allTeams:
+        if match["homeClub"] == team:
+            dublicate = True
+            break
+        else:
+            dublicate = False
+    if dublicate == False:
+        allTeams.append(match["homeClub"])
+
+
 
 # wins by home team
 homeWins =0
@@ -68,12 +81,13 @@ def matchResultsHome(homeClub):
             loses = loses + 1
         if match["homeClub"] == homeClub and match["homeScore"] < match["guestScore"]:
             draws = draws + 1
+
     print(homeClub + " : %s" % wins)
     print(homeClub + " : %s" % loses)
     print(homeClub + " : %s" % draws)
 
 print("machtes of each team as homeClub: ")
-for team in teams:
+for team in allTeams:
     matchResultsHome(team)
 
 # matches of each team as guestClub
@@ -88,12 +102,13 @@ def matchResultsGuest(guestClub):
             loses = loses + 1
         if match["guestClub"] == guestClub and match["guestScore"] < match["homeScore"]:
             draws = draws + 1
+
     print(guestClub + " : %s" % wins)
     print(guestClub + " : %s" % loses)
     print(guestClub + " : %s" % draws)
 
 print("machtes of each team as guestClub: ")
-for team in teams:
+for team in allTeams:
     matchResultsGuest(team)
 
 # against which team were the most wins/loses/draws as homeclub
@@ -107,7 +122,7 @@ def specificMatches (homeClub):
     wins = 0
     loses = 0
     draws = 0
-    for team in teams:
+    for team in allTeams:
         for match in fetchedMatches:
             if match["homeClub"] == homeClub and match["guestClub"] == team and match["guestScore"] < match["homeScore"]:
                 wins = wins +1
