cf9e351fe59dc09d6d7154b65950ec82462e0217
Pedroxb <56825499+mastergundyr@users.noreply.github.com>
2021-12-06 18:53:16 +0100

    main funktions done

--- teamproject/gui.py
+++ teamproject/gui.py
@@ -22,25 +22,15 @@ import json
 from teamproject.models import BaselineAlgo
 from teamproject.models import PoissonRegression
 from teamproject.crawler import get_data
+import pandas as pd
 
 
-
-#open the json file with all the matches, then load this data into the BaselineAlgo
-f = open('teamproject/matches.json',)
-data = json.load(f)
-model = BaselineAlgo(data)
-#f.close()
   
 
 def main():
     """
     Creates and shows the main window.
     """
-
-    f = open('teamproject/matches.json',)
-    hardloaddata = json.load(f)
-    model = BaselineAlgo(hardloaddata)
-    
     
     # Add code here to create and initialize window.
     class Ui_Dialog(object):
@@ -81,7 +71,7 @@ def main():
             self.StartDaycomboBox.setObjectName("StartDaycomboBox")
             
             #A year has 365 days, so put 1-365 into the combobox
-            for x in range(1,366):
+            for x in range(0,366):
                 self.StartDaycomboBox.addItem(str(x))
 
             #The combobox for the end year
@@ -99,51 +89,59 @@ def main():
             self.EndDaycomboBox.setObjectName("EndDaycomboBox")
 
             #A year has 365 days, so put 1-365 into the combobox
-            for x in range(1,366):
+            for x in range(0,366):
                 self.EndDaycomboBox.addItem(str(x))
             
-             #Select the teams label
-            self.SelectTeamLabel = QtWidgets.QLabel(Dialog)
-            self.SelectTeamLabel.setGeometry(QtCore.QRect(30, 300, 371, 31))
-            self.SelectTeamLabel.setObjectName("SelectTeamLabel")
-
-            # homecomboBox for the home team
-            self.homecomboBox = QtWidgets.QComboBox(Dialog)
-            self.homecomboBox.setGeometry(QtCore.QRect(60, 380, 301, 61))
-            self.homecomboBox.setObjectName("homecomboBox")
-            
-            #Loop through the json file and get all the home clubs, this doesn't account for duplicates yet. 
-            for team in data:
-                self.homecomboBox.addItem(team['homeClub'])
-            
-            #Guest Combobox for the guest team
-            self.guestcomboBox = QtWidgets.QComboBox(Dialog)
-            self.guestcomboBox.setGeometry(QtCore.QRect(570, 380, 301, 61))
-            self.guestcomboBox.setObjectName("guestcomboBox")
-            
-            #Result label
-            self.resultLabel = QtWidgets.QLabel(Dialog)
-            self.resultLabel.setGeometry(QtCore.QRect(410, 680, 371, 31))
-            self.resultLabel.setObjectName("resultLabel")
 
             #Crawler Button
             self.crawlerbutton = QtWidgets.QPushButton(Dialog)
-            self.crawlerbutton.setGeometry(QtCore.QRect(0, 480, 331, 101))
+            self.crawlerbutton.setGeometry(QtCore.QRect(50, 250, 331, 101))
             self.crawlerbutton.setObjectName("Activate Crawler")
             self.crawlerbutton.clicked.connect(self.crawlercall)
+
+            #Select the teams label
+            self.SelectTeamLabel = QtWidgets.QLabel(Dialog)
+            self.SelectTeamLabel.setGeometry(QtCore.QRect(30, 380, 371, 31))
+            self.SelectTeamLabel.setObjectName("SelectTeamLabel")
+
+            #HomecomboBox for the home team
+            self.homecomboBox = QtWidgets.QComboBox(Dialog)
+            self.homecomboBox.setGeometry(QtCore.QRect(60, 430, 301, 61))
+            self.homecomboBox.setObjectName("homecomboBox")
             
+            #Guest Combobox for the guest team
+            self.guestcomboBox = QtWidgets.QComboBox(Dialog)
+            self.guestcomboBox.setGeometry(QtCore.QRect(570, 430, 301, 61))
+            self.guestcomboBox.setObjectName("guestcomboBox")
+            
+            #Select the Algo label
+            self.SelectAlgoLabel = QtWidgets.QLabel(Dialog)
+            self.SelectAlgoLabel.setGeometry(QtCore.QRect(30, 510, 371, 31))
+            self.SelectAlgoLabel.setObjectName("SelectAlgoLabel")
+
+            #Algocombobox: Select the algorithm you want to use
+            self.algocomboBox = QtWidgets.QComboBox(Dialog)
+            self.algocomboBox.setGeometry(QtCore.QRect(60, 570, 301, 61))
+            self.algocomboBox.setObjectName("algocomboBox")
+            self.algocomboBox.addItem("Baseline Algorithm")
+            self.algocomboBox.addItem("Poisson Regression Algorithm")
         
             #Training button
             self.trainingbutton = QtWidgets.QPushButton(Dialog)
-            self.trainingbutton.setGeometry(QtCore.QRect(330, 480, 331, 101))
+            self.trainingbutton.setGeometry(QtCore.QRect(100, 660, 331, 101))
             self.trainingbutton.setObjectName("Start training")
             self.trainingbutton.clicked.connect(self.trainingcall)
             
-            
+            #Result Button
             self.resultsbutton = QtWidgets.QPushButton(Dialog)
-            self.resultsbutton.setGeometry(QtCore.QRect(660, 480, 331, 101))
+            self.resultsbutton.setGeometry(QtCore.QRect(430, 660, 331, 101))
             self.resultsbutton.setObjectName("Show results")
             self.resultsbutton.clicked.connect(self.resultscall)
+
+            #Result label
+            self.resultLabel = QtWidgets.QLabel(Dialog)
+            self.resultLabel.setGeometry(QtCore.QRect(410, 780, 371, 31))
+            self.resultLabel.setObjectName("resultLabel")
 
             #call the retranslate
             self.retranslateUi(Dialog)
@@ -156,45 +154,79 @@ def main():
             
             QtCore.QMetaObject.connectSlotsByName(Dialog)
         
+        def filterdata(self):
+            # this uses the get_data funktion out of the crawler.py to get the data in the time you chose in the comboboxes
+            data = get_data(int(self.StartYearcomboBox.currentText()),int(self.StartDaycomboBox.currentText()), int(self.EndYearcomboBox.currentText()),int(self.EndDaycomboBox.currentText()))
+            
+            
+            #filter out duplicates and sort after the homeClub/guestclub
+            guestdata= data[['guestClub']].drop_duplicates()
+            homedata= data[['homeClub']].drop_duplicates()
+
+            #Transform the data into a list
+            guestdata = guestdata[['guestClub']].values.tolist()
+            homedata = homedata[['homeClub']].values.tolist()
+            
+
+            #this loops through the GuestClubs and write them into the guestCombobox as options
+            for team in guestdata:
+                self.guestcomboBox.addItem(team[0])
+        
+            #this loops through the HomeClubs and write them into the guestCombobox as options
+            for team in homedata:
+                self.homecomboBox.addItem(team[0])
+            
+            return data
         #this will get called when you press the activate crawler button
         def crawlercall(self):
-            self.crawlerbutton.setText("Crawler running")
-            #textboxValue = self.textbox.text()
-            data = get_data(int(self.StartYearcomboBox.currentText()),int(self.StartDaycomboBox.currentText()), int(self.EndYearcomboBox.currentText()),int(self.EndDaycomboBox.currentText()))
-            print(data.index)
-
-              #Loop through the json file and get all the guest clubs, this doesn't account for duplicates yet. 
-            for team in data.index:
-                self.guestcomboBox.addItem(data.team['guestClub'])
-
+            
+            #change text of the crawler button
+            self.crawlerbutton.setText("Crawler finished collecting data")
+            
+            self.filterdata()
         
-        #this will get called when you press the Start training button
-        def trainingcall(self):
-            pass
+        #this funktion trains the algo with the crawled data.
+        def trainAlgo(self):
+            if self.algocomboBox.currentText() == "Baseline Algorithm":
+                model = BaselineAlgo(self.filterdata())
+                print("Baseline Algorithm")
             
-
-        #this will get called when you press the Show results button, I think i will do a popup with the winner.
-        def resultscall(self):
-
-            #printing for test purposes
-            print(self.homecomboBox.currentText())
-
-            #predict the Winner out of the 2 teams chosen in the comboboxes
-            winner = model.predict_winner(str(self.homecomboBox.currentText()), str(self.guestcomboBox.currentText()))
+            elif self.algocomboBox.currentText() == "Poisson Regression Algorithm":
+                model = PoissonRegression(self.filterdata())
+                print("Poisson Regression Algorithm")
             
-            #Test
-            print(winner)
-
-            f.close()
+            predictionlist = model.predict_winner(str(self.homecomboBox.currentText()), str(self.guestcomboBox.currentText()))
+    
             """
             If winner[0] (which is the home win percentage) is higher then the winner[2] (which is the guest winner percentage), 
             then display the home team in the button text. Else display the guest team. I will probably change this to 
             a popup window at some point.
             """
-            if winner[0] > winner[2]:
-                self.resultLabel.setText("Results: " + self.homecomboBox.currentText())
+            if predictionlist[0] > predictionlist[2]:
+                winner = "homeClub"
             else:
-                self.resultLabel.setText("Results: " + self.guestcomboBox.currentText())
+                winner = "guestClub"
+
+            return winner
+       
+        #this will get called when you press the Start training button
+        def trainingcall(self):
+
+            self.trainAlgo()
+            self.trainingbutton.setText("Training finished")
+
+        #this will get called when you press the Show results button, I think i will do a popup with the winner.
+        def resultscall(self):
+            
+            winner = self.trainAlgo()
+
+            #predict the Winner out of the 2 teams chosen in the comboboxes
+            if winner == "homeClub":
+                self.resultLabel.setText("Results:  " + self.homecomboBox.currentText()+ " will win")
+
+            else:
+                self.resultLabel.setText("Results:  " + self.guestcomboBox.currentText()+ " will win")
+           
 
 
 
@@ -213,6 +245,7 @@ def main():
             self.trainingbutton.setText(_translate("Dialog", "Start training"))
             self.SelectTeamLabel.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:11pt;\">Select the home team and the guest team:</span></p></body></html>"))
             self.SelectStartTimeLabel.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:11pt;\">Select the start year and day:</span></p></body></html>"))
+            self.SelectAlgoLabel.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:11pt;\">Select the algorithm you want to use:</span></p></body></html>"))
             self.SelectEndTimeLabel.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:11pt;\">Select the end year and day:</span></p></body></html>"))
             self.resultLabel.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:11pt;\">Results:</span></p><p><br/></p></body></html>"))
 
